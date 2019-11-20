40d796fe63100d71bbe957ea26df6e240bf4de8d
bilalashraf1710 <bilalashraf1710@gmail.com>
2019-11-21 03:48:00 +0500

    Updates

---
+++ precache-manifest.c8a053805fabe903ae47bbbec68dcfc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd73e2ea1141c3792e87da83d46f182e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1df0547fb82bbf83f61c",
+    "url": "/monsters-rolodex/static/css/main.ab9bfc60.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "1df0547fb82bbf83f61c",
+    "url": "/monsters-rolodex/static/js/main.8ca1063d.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
